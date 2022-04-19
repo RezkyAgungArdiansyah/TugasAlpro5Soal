0bd150bf23a583ef90b0437fda2ac51210359df4
RezkyAgungArdiansyah <rizkyagungra1234@gmail.com>
2022-04-19 19:57:57 +0700

    tugas

---
+++ src/tugas5soal/SoalNO1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas5soal;
+
+import java.util.Scanner;
+
+/**
+ * 
+ *
+ * @author Lenovo
+ */
+public class SoalNO1 {            
+            
+    public static void main (String[]args){
+    Scanner inputan = new Scanner(System.in);
+    boolean nilai = true;
+    while (nilai){
+    System.out.println (" *Program Test bilangan Prima* Masukkan angka yang mau anda cek ");
+    int YangDicari = inputan.nextInt();
+    if  (YangDicari >= 2){
+    System.out.println("Program Sah");
+    }
+    }
+     
+}}
